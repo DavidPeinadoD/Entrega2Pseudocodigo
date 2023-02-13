b457183d7de59bd5fa4f9bd027007e16f2f77597
DavidPeinadoD <113924648+DavidPeinadoD@users.noreply.github.com>
2023-02-13 13:59:50 +0000

    ejercicio 2 hecho

---
+++ Pregunta2.java
@@ -0,0 +1,15 @@
+/*
+Algo QueVeremos 
+Variable valor1, valor2: entero  
+Variable cadena1: texto  
+Constante CST: real <- 49,78  
+Inicio  
+   valor1 <- 92 % 8  
+   valor2 <- 2 * valor1  
+   cadena1 <- "Test"  
+   escribir(cadena1 & " , valor2 = " & valor2)  
+   escribir(valor1 & " # " & CST)  
+Fin */
+//¿Qué veremos al ejecutar el siguiente algoritmo?  
+// Test , valor2 = 4
+// 2 # 49.78
