935868a50f428cf1362d83289b72d8907a53240d
DavidPeinadoD <113924648+DavidPeinadoD@users.noreply.github.com>
2023-02-13 13:55:25 +0000

    Ejercicio 1 hecho

---
+++ Pregunta1.java
@@ -0,0 +1,15 @@
+
+/*Algo ValoresDeLasVariables  
+variable a, b: entero  
+Inicio  
+   a <- 3  
+   escribir("a = " & a)  
+   b<-a+5  
+   escribir("a = " & a & " y b = " & b)  
+   a<-7  
+   escribir("a = " & a & " y b = " & b)  
+Fin */
+
+//Cuáles serán los valores de las variables a y b después de cada línea del siguiente algoritmo
+
+//a) a = 3, b = 8, a = 7, b = 8
