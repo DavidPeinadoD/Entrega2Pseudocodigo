c323f790f8caf392d065628da542479313ae7781
DavidPeinadoD <113924648+DavidPeinadoD@users.noreply.github.com>
2023-02-13 14:05:02 +0000

    ejercicio 3 hecho

---
+++ Pregunta3.java
@@ -0,0 +1,11 @@
+/*Variable valor1, valor2: real  
+Inicio  
+   valor1 <- enter("Introduza un valor: ")  
+   valor2 <- enter("Introduza otro valor: ")  
+   # tratamiento 
+   valor1 <- valor2  
+   valor2 <- valor1  
+   escribir("valor1 = " & valor1 & "; valor2 = " & valor2)  
+Fin */
+//¿Qué veremos al ejecutar el siguiente algoritmo?
+// Veremos como los valores de que hemos introducido se cambian entre si, si introducimos como valor 1 -> 4 e introducimoscomo valor 2 -> 3, al final del algoritmo, tendremmos que el valor 1 = 3 y el valor 2 = 4 
